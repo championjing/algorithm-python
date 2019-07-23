5b7fabc5617232983c400636214d21736fd27400
champion <championjing@163.com>
2019-07-23 11:20:44 +0800

    移动文件夹

--- test.py
+++ test.py
@@ -1,26 +1,3 @@
-# coding=UTF-8
-test_list = [ 1, 6, 3, 5, 3, 4 ,98,2,14,54,3,132,7] 
-  
-# print("查看 4 是否在列表中 ( 使用循环 ) : ") 
-  
-for n in test_list: 
-    if(n == 4) : 
-        print ("yes") 
-  
-# print("查看 4 是否在列表中 ( 使用 in 关键字 ) : ") 
-
-if (4 in test_list): 
-    print ("yes") 
-
-def bubling(arr):
-        for i in range(len(arr)):
-                for j in range(len(arr)-i-1):
-                        if( arr[j] > arr[j+1] ):
-                                temp = arr[j]
-                                arr[j] = arr[j+1]
-                                arr[j+1] = temp
-        return arr
-
-arr1 = [1,2]
-arr2 = [4,3]
-print ( str( arr1 + arr2 ))
+#encoding=utf-8
+res = [0]*3
+print( str(res) )
